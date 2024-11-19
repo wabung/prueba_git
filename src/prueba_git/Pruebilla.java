0e53089be7d8ed20a0fb1c10f9d6e9629914df6c
Mario García Marín <mgarmar2604@g.educaand.es>
2024-11-19 09:53:51 +0100

    adicion del sexo

--- src/prueba_git/Pruebilla.java
+++ src/prueba_git/Pruebilla.java
@@ -4,8 +4,8 @@ public class Pruebilla {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo.");
-
+		System.out.println("Hola mundo. sexo");
+		
 	}
 
 }
